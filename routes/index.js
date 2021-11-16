216f230bdea3a7c260e1cf02ee083bb56c62f881
asbelljc <asbelljc@gmail.com>
2021-11-16 01:29:54 -0500

    Basic build done with no styling yet.

---
+++ routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Mini Message Board', messages: messages });
+});
+
+router.get('/new', (req, res, next) => {
+  res.render('form');
+});
+
+router.post('/new', (req, res, next) => {
+  const text = req.body.message;
+  const user = req.body.name;
+  const added = new Date();
+
+  messages.push({ text, user, added });
+
+  res.redirect('/');
+});
+
+module.exports = router;
